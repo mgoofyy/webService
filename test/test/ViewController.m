6c8d0ddb91fd6c617fb46ce4f85030b37134e512
mgoofyy <mgoofyy@gmail.com>
2016-04-30 09:28:22 +0800

    test3

--- test/test/ViewController.m
+++ test/test/ViewController.m
@@ -15,6 +15,8 @@
 @implementation ViewController
 
 - (void)viewDidLoad {
+    
+    NSLog(@"___________%s",__func__);
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 }
